2f82c26d3aaf968e700d2a054f2f3a4e91648486
Aung Maw <aungmawjj12@gmail.com>
2024-08-19 22:46:47 +0800

    Add react router
    Remove daisy ui to simplify

--- webapp/src/repository/localRepo.test.ts
+++ webapp/src/repository/localRepo.test.ts
@@ -1,5 +1,5 @@
 import { Routine, RoutineRecord, User } from '../types';
-import LocalRepo from './LocalRepo';
+import localRepo from './localRepo';
 
 const mockUser: User = {
   Id: '123',
@@ -43,49 +43,49 @@ const mockRecords: RoutineRecord[] = [
 
 describe('CurrentUser', () => {
   test('Get undefined', async () => {
-    const user = await LocalRepo.getCurrentUser();
+    const user = await localRepo.getCurrentUser();
     expect(user).toBeUndefined();
   });
 
   test('Put User', async () => {
-    await LocalRepo.putCurrentUser(mockUser);
-    const user = await LocalRepo.getCurrentUser();
+    await localRepo.putCurrentUser(mockUser);
+    const user = await localRepo.getCurrentUser();
     expect(user).toEqual(mockUser);
   });
 
   test('Delete User', async () => {
-    await LocalRepo.deleteCurrentUser();
-    const user = await LocalRepo.getCurrentUser();
+    await localRepo.deleteCurrentUser();
+    const user = await localRepo.getCurrentUser();
     expect(user).toBeUndefined();
   });
 });
 
 describe('Routine', () => {
   test('No Routines', async () => {
-    const routines = await LocalRepo.getRoutines(mockUser.Id);
+    const routines = await localRepo.getRoutines(mockUser.Id);
     expect(routines).toEqual([]);
   });
 
   test('Put Routine', async () => {
-    await LocalRepo.putRoutine(mockRoutines[0]);
-    await LocalRepo.putRoutine(mockRoutines[1]);
+    await localRepo.putRoutine(mockRoutines[0]);
+    await localRepo.putRoutine(mockRoutines[1]);
 
-    const routines = await LocalRepo.getRoutines(mockUser.Id);
+    const routines = await localRepo.getRoutines(mockUser.Id);
     expect(routines.length).toBe(2);
     expect(routines).toContainEqual(mockRoutines[0]);
     expect(routines).toContainEqual(mockRoutines[1]);
   });
 
   test('Different User', async () => {
-    const routines = await LocalRepo.getRoutines('different_user');
+    const routines = await localRepo.getRoutines('different_user');
     expect(routines).toEqual([]);
   });
 
   test('Update Routine', async () => {
     const update: Routine = { ...mockRoutines[0], Note: 'some update' };
-    await LocalRepo.putRoutine(update);
+    await localRepo.putRoutine(update);
 
-    const routines = await LocalRepo.getRoutines(mockUser.Id);
+    const routines = await localRepo.getRoutines(mockUser.Id);
     expect(routines.length).toBe(2);
     expect(routines).not.toContainEqual(mockRoutines[0]);
     expect(routines).toContainEqual(mockRoutines[1]);
@@ -95,22 +95,22 @@ describe('Routine', () => {
 
 describe('Routine Record', () => {
   test('No Records', async () => {
-    const records = await LocalRepo.getRoutineRecords(mockUser.Id, 1, 2);
+    const records = await localRepo.getRoutineRecords(mockUser.Id, 1, 2);
     expect(records).toEqual([]);
   });
 
   test('Put Record', async () => {
-    await LocalRepo.putRoutineRecord(mockRecords[0]);
-    await LocalRepo.putRoutineRecord(mockRecords[1]);
+    await localRepo.putRoutineRecord(mockRecords[0]);
+    await localRepo.putRoutineRecord(mockRecords[1]);
 
-    const records = await LocalRepo.getRoutineRecords(mockUser.Id, 1, 2);
+    const records = await localRepo.getRoutineRecords(mockUser.Id, 1, 2);
     expect(records.length).toBe(2);
     expect(records).toContainEqual(mockRecords[0]);
     expect(records).toContainEqual(mockRecords[1]);
   });
 
   test('Get By Range', async () => {
-    const records = await LocalRepo.getRoutineRecords(mockUser.Id, 0, 1);
+    const records = await localRepo.getRoutineRecords(mockUser.Id, 0, 1);
     expect(records.length).toBe(1);
   });
 });
